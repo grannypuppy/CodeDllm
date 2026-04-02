9de91e633266a910f7c1fb69aa451a6767d86c01
jiamin0630 <aboatwithflow@gmail.com>
2026-04-01 19:22:42 -0500

    rl multitask

--- run_rl_multitask.sh
+++ run_rl_multitask.sh
@@ -17,6 +17,3 @@ python eval/batch_eval.py \
 
 python rl.py \
   config=configs/rl_dream_py_multitask.yaml \
-  experiment.run_name=sft-dream-multitask-ckpt1000-rl-2lm-8ga-4gpus-rklw0.0 \
-  training.rank_loss_weight=0.0 \
-  reward.baseline_results_path=results/baseline/baseline/batch_results.jsonl
